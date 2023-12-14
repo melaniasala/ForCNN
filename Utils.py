a7386cc02009fbbaefb988e73c1442df973be971
melaniasala <melania.sala@mail.polimi.it>
2023-12-14 19:31:24 +0100

    normalize_window() now supports single serie sample as input

--- Utils.py
+++ Utils.py
@@ -2,11 +2,6 @@
 import tensorflow as tf
 from tensorflow import keras as tfk
 from tensorflow.keras import layers as tfkl
-tf.autograph.set_verbosity(0)
-tf.get_logger().setLevel(logging.ERROR)
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-tf.random.set_seed(seed)
-tf.compat.v1.set_random_seed(seed)
 
 import pandas as pd
 import seaborn as sns
@@ -14,24 +9,47 @@ import numpy as np
 
 from datetime import datetime
 import matplotlib.pyplot as plt
-plt.rc('font', size=16)
-from sklearn.preprocessing import MinMaxScaler
 
 
 
 def normalize_window(windows_batch, return_minmax= False):
-    # squeeze last dimension
-    windows_batch =np.squeeze(windows_batch)
+    """
+    Normalize a batch of time series windows using min-max scaling.
+
+    Parameters:
+    - windows_batch (numpy.ndarray):    A batch of time series windows with shape (batch_size, window_length, 1)
+                                        or (window_length, 1) for a single sample in the batch.
+    - return_minmax (bool, optional):   If True, return both the scaled batch and the min-max array for each sample.
+                                        If False, return only the scaled batch. Default is False.
+
+    Returns:
+    - numpy.ndarray or tuple: If return_minmax is False, returns the scaled batch.
+                              If return_minmax is True, returns a tuple (scaled_batch, minmax_batch_array).
+
+    """
+
+    single_sample = False
+    if len(windows_batch.shape) == 2:
+        single_sample = True
+
+    # Squeeze last dimension
+    windows_batch = np.squeeze(windows_batch)
     batch_size = len(windows_batch)
 
     # For each sample compute min and max value
-    minmax_batch_array = np.array([[np.min(windows_batch[i]), np.max(windows_batch[i])] for i in range(batch_size)])
-
+    if single_sample:
+        minmax_batch_array = np.array([[np.min(windows_batch)], [np.max(windows_batch)]])
+    else:
+        minmax_batch_array = np.array([[np.min(windows_batch[i]), np.max(windows_batch[i])] for i in range(batch_size)])
+   
     # Perform min-max scaling for each sample
-    scaled_batch = np.array([(windows_batch[i] - minmax_batch_array[i,0]) / (minmax_batch_array[i,1] - minmax_batch_array[i,0]) for i in range(batch_size)])
+    if single_sample:
+        scaled_batch = np.array([(windows_batch - minmax_batch_array[0]) / (minmax_batch_array[1] - minmax_batch_array[0])])
+    else:
+        scaled_batch = np.array([(windows_batch[i] - minmax_batch_array[i, 0]) / (minmax_batch_array[i, 1] - minmax_batch_array[i, 0]) for i in range(batch_size)])
 
+    # Return results
     if return_minmax:
         return scaled_batch, minmax_batch_array
-
     else:
         return scaled_batch
